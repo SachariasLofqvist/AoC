11b1d705d150621d8201f7c695ddaebb2eda317d
Sacharias Löfqvist <115622846+SachariasLofqvist@users.noreply.github.com>
2024-12-05 17:25:12 +0100

    Update Day2_pt2.py

--- Day2/Day2_pt2.py
+++ Day2/Day2_pt2.py
@@ -86,16 +86,3 @@ for item in input_list:
         safe += 1
 
 print(safe)
-
-# count = 0
-
-# def safe(levels):
-#     diffs = [x - y for x, y in zip(levels, levels[1:])]
-#     return all(1 <= x <= 3 for x in diffs) or all(-1 >= x >= -3 for x in diffs)
-
-# for report in open("Input.txt"):
-#     levels = list(map(int, report.split()))
-#     if any(safe(levels[:index] + levels[index + 1:] for index in range(len(levels)))):
-#         count += 1
-
-# print(count)
